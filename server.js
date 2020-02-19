2f1737eef7e99a26b090c60f67fc9194773f8ef9
shimmy77 <christianshim@gmail.com>
2020-02-19 11:09:20 -0600

    finished lol

--- server.js
+++ server.js
@@ -4,6 +4,48 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 
- console.log(process.env.API_TOKEN)
+const moviedex = require('./movies-data-small.json')
+
+ //console.log(process.env.API_TOKEN)
 
  const app = express()
+
+ app.use(morgan('dev'))
+ app.use(helmet())
+ app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+
+    next()
+})
+
+ app.get('/movie', function handleGetMovie(req, res) {
+    let response = moviedex;
+
+    if(req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+    } 
+    
+    if(req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >= Number(req.query.avg_vote))
+    }
+    res.json(response)
+ })
+ 
+
+ app.listen(8000, () => {
+     console.log('Listening at port 8000')
+ })
