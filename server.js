2f39b93526cb91600d1011951470cbd0ac59be8d
AveraqeDev <averaqedev@gmail.com>
2020-02-21 11:31:45 -0800

    prepare for deployment

--- server.js
+++ server.js
@@ -1,51 +1,62 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const moviedex = require('./movies-data-small.json')
+const moviedex = require('./movies-data-small.json');
 
- //console.log(process.env.API_TOKEN)
+//console.log(process.env.API_TOKEN)
 
- const app = express()
+const app = express();
 
- app.use(morgan('dev'))
- app.use(helmet())
- app.use(cors())
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-    if(!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({error: 'Unauthorized request'})
-    }
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
 
-    next()
-})
+  next();
+});
 
- app.get('/movie', function handleGetMovie(req, res) {
-    let response = moviedex;
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = moviedex;
 
-    if(req.query.genre) {
-        response = response.filter(movie => 
-            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
-    } 
+  if(req.query.genre) {
+    response = response.filter(movie => 
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+  } 
     
-    if(req.query.country) {
-        response = response.filter(movie =>
-            movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
-    }
+  if(req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  }
 
-    if(req.query.avg_vote) {
-        response = response.filter(movie =>
-            Number(movie.avg_vote) >= Number(req.query.avg_vote))
-    }
-    res.json(response)
- })
+  if(req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote));
+  }
+  res.json(response);
+});
  
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
- app.listen(8000, () => {
-     console.log('Listening at port 8000')
- })
+app.listen(process.env.PORT, () => {
+  console.log('Listening at port 8000');
+});
